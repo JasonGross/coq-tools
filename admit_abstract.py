3b3645da3871e4bc889dd75ba9971e0967c02aa8
Jason Gross <jgross@mit.edu>
2013-04-20 10:55:33 -0400

    Add more logging to admit abstracts, fix a bug in readiness

--- admit_abstract.py
+++ admit_abstract.py
@@ -27,13 +27,17 @@ def transform_abstract_to_admit_statement(statement, agressive=False, verbose=1,
                 if term == ';':
                     if agressive:
                         rtn.append(' admit;')
+                        if verbose >= 3: log("Appending ' admit;' to rtn")
                     else:
                         cur.append(term)
                         rtn.append(''.join(cur))
+                        if verbose >= 3: log("Appending %s to rtn" % repr(''.join(cur)))
                 else:
+                    if verbose >= 3: log("Appending ' admit.' to rtn")
                     rtn.append(' admit.')
                 in_abstract = False
                 cur = []
+                if verbose >= 3: log("Setting in_abstract to false and emptying cur")
             elif abstract_paren_level < 0:
                 log('Warning: abstract_paren_level messed up on statement %s' % repr(statement))
                 in_abstract = False
@@ -41,25 +45,32 @@ def transform_abstract_to_admit_statement(statement, agressive=False, verbose=1,
                 rtn.append(''.join(cur))
                 cur = []
                 abstract_paren_level = 0
+                if verbose >= 3: log("Setting in_abstract to false, abstract_paren_level to 0, emptying cur, and\nappending %s to rtn" %
+                                     repr(''.join(cur)))
             else:
                 if term == '(':
                     abstract_paren_level += 1
                 elif term == ')':
                     abstract_paren_level -= 1
                 cur.append(term)
+                if verbose >= 3: log("Setting abstract_paren_level to %d and\nappending %s to cur" % repr(term))
         else:
             if ready_for_abstract and term.strip() == 'abstract':
                 cur.append(term)
                 in_abstract = True
+                if verbose >= 3: log("Found %s (appending to cur), now in_abstract" % repr(term))
                 if abstract_paren_level != 0:
                     log('Warning: abstract_paren_level messed up before abstract on statement %s' % repr(statement))
                     abstract_paren_level = 0
             else:
-                if term.strip():
-                    ready_for_abstract = False
-                elif term in '.;':
+                if term in '.;':
+                    if verbose >= 3: log("Found %s (appending to rtn), ready for abstract" % repr(term))
                     ready_for_abstract = True
+                elif term.strip():
+                    if verbose >= 3: log("Found %s (appending to rtn), not ready for abstract" % repr(term))
+                    ready_for_abstract = False
                 rtn.append(term)
+    if verbose >= 3: log("Done.  Appending %s to rtn." % repr(''.join(cur)))
     rtn.append(''.join(cur))
 
     return ''.join(rtn)
