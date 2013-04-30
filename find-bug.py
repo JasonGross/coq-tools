fa008ba4048403d7531b938b7c4c553fdb0458cf
Jason Gross <jgross@mit.edu>
2013-04-30 15:20:16 -0400

    Memoize more regular expressions

--- find-bug.py
+++ find-bug.py
@@ -76,6 +76,10 @@ def read_from_file(file_name):
 def re_compile_printf(pattern_string, values, *args):
     return re.compile(pattern_string % values, *args)
 
+# memoize the compilation
+def re_search(pattern, string, flags=0):
+    return re_compile_printf(pattern, flags).search(string)
+
 def get_error_reg_string(output_file_name, verbose=DEFAULT_VERBOSITY, log=DEFAULT_LOG):
     error_reg_string = ''
     while error_reg_string == '':
@@ -247,7 +251,7 @@ def try_remove_if_name_not_found_in_transformer(get_names, verbose=DEFAULT_VERBO
         elif EXCLUSION_REG.search(future_definition['statement']):
             return False # we don't care if the name is found in a
                          # statement like [Section ...] or [End ...]
-        return any(re.search(r"(?<![\w'])%s(?![\w'])" % name, future_definition['statement'])
+        return any(re_search(r"(?<![\w'])%s(?![\w'])" % name, future_definition['statement'])
                    for name in names)
     return try_remove_if_not_matches_transformer(definition_found_in, verbose=verbose, log=log)
 
@@ -267,7 +271,7 @@ def try_remove_if_name_not_found_in_section_transformer(get_names, verbose=DEFAU
                 section_level += 1
             elif SECTION_END_REG.match(future_definition['statement']):
                 section_level -= 1
-            elif any(re.search(r"(?<![\w'])%s(?![\w'])" % name, future_definition['statement'])
+            elif any(re_search(r"(?<![\w'])%s(?![\w'])" % name, future_definition['statement'])
                      for name in names):
                 return cur_definition
         # we didn't find the name, so we can safely remove it
