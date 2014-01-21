68ae58b897210b2940ace3f5815dd3e1b5a91a63
Jason Gross <jgross@mit.edu>
2014-01-21 14:45:42 -0500

    Remove prints, fix ex2 for Coq 8.3pl4

--- examples/example_2/A/A.v
+++ examples/example_2/A/A.v
@@ -2,6 +2,8 @@ Require A.A_0.
 Require A.A_1.
 Require A.A_2.
 
-Include A.A_0.
-Include A.A_1.
-Include A.A_2.
+Module Export A_A_foo.
+  Include A.A_0.
+  Include A.A_1.
+  Include A.A_2.
+End A_A_foo.
--- examples/example_2/B/B.v
+++ examples/example_2/B/B.v
@@ -2,6 +2,8 @@ Require B.B_0.
 Require B.B_1.
 Require B.B_2.
 
-Include B.B_0.
-Include B.B_1.
-Include B.B_2.
+Module Export B_B_foo.
+  Include B.B_0.
+  Include B.B_1.
+  Include B.B_2.
+End B_B_foo.
