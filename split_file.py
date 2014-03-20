5e7350610188248afc75639d34b424a44469f5cf
Jason Gross <jgross@mit.edu>
2014-03-20 19:50:02 -0400

    Don't split on '..', only '...' or '.'

--- split_file.py
+++ split_file.py
@@ -6,7 +6,7 @@ __all__ = ["split_coq_file_contents"]
 def split_coq_file_contents(contents):
     """Splits the contents of a coq file into multiple statements.
 
-    This is done by finding periods followed by whitespace.  This is a
-    dumb algorithm, but it seems to be (nearly) the one that
-    ProofGeneral and CoqIDE use."""
-    return re.split('(?<=\.)\s', strip_comments(contents))
+    This is done by finding one or three periods followed by
+    whitespace.  This is a dumb algorithm, but it seems to be (nearly)
+    the one that ProofGeneral and CoqIDE use."""
+    return re.split('(?<=[^\.]\.\.\.)\s|(?<=[^\.]\.)\s', strip_comments(contents))
