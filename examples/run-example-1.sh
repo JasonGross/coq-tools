2270e85b153a4a69b4c3d1d03c5f5a6c538734dc
Jason Gross <jgross@mit.edu>
2014-05-01 11:47:35 -0400

    Simplify examples

--- examples/run-example-1.sh
+++ examples/run-example-1.sh
@@ -4,4 +4,4 @@ cd "$DIR/.."
 PS4='$ '
 set -x
 # Use absolute paths because --directory messes with relative paths
-python ./find-bug.py --fast-merge-imports --strip-newlines 2 --directory="$(readlink -f ./examples/example_1)" "$(readlink -f ./examples/example_1/C.v)" "$(readlink -f ./examples/example_1_output.v)" "$@" -l - "$(readlink -f ./examples/example_1_log.log)"
+python ./find-bug.py --directory="$(readlink -f ./examples/example_1)" "$(readlink -f ./examples/example_1/C.v)" "$(readlink -f ./examples/example_1_output.v)" "$@" -l - "$(readlink -f ./examples/example_1_log.log)"
