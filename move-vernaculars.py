8846eeeda371e4faefc911e8fbf3e8e8ede86ac7
Jason Gross <jgross@mit.edu>
2015-03-09 00:31:31 -0400

    Better minimization algorithm for opacity

--- move-vernaculars.py
+++ move-vernaculars.py
@@ -94,6 +94,51 @@ def strip_parens(string):
         last, cur = cur, re.sub('"[^"]+|{[^{}]+}|\([^\(\)]+\)', '', cur)
     return cur
 
+
+def space_canonicalize(stmt):
+    return re.sub(r'\s+', ' ', stmt.strip(' \t\n\r.'))
+
+def canonicalize(stmt):
+    return space_canonicalize(stmt).replace('Transparent ', 'Opaque ')
+
+def cancels(statement1, statement2):
+    """Returns True if statement2 cancels the effect of statement1; False otherwise"""
+    return canonicalize(statement1) == canonicalize(statement2)
+
+def commutes(statement1, statement2):
+    if any(i in statement1 or i in statement2 for i in ('(*', '*)', '"')):
+        return False
+    statement1, statement2 = space_canonicalize(statement1), space_canonicalize(statement2)
+    m1 = re.match(r'^(?:Local |Global )?(?:Transparent|Opaque) (.*)$', statement1)
+    m2 = re.match(r'^(?:Local |Global )?(?:Transparent|Opaque) (.*)$', statement2)
+    if not m1 or not m2:
+        return False
+    constants1, constants2 = set(m1.groups()[0].split(' ')), set(m2.groups()[0].split(' '))
+    return constants1.isdisjoint(constants2)
+
+def preminimize_lifted_statements(statements):
+    """Remove useless deferred statements, such as transparent followed by opaque"""
+
+    prev = set()
+    for statement in statements:
+        prev = set(last for last in prev if not cancels(last, statement))
+        if not all(commutes(last, statement) for last in prev):
+            for last in sorted(prev):
+                yield last
+            prev = set()
+        prev.add(statement)
+    for last in sorted(prev):
+        yield last
+
+
+def minimize_lifted_statements(statements):
+    statements = list(preminimize_lifted_statements(statements))
+    sans_opacity = [i for i in statements if re.match(r'^(?:Local\s+|Global\s+)?(?:Transparent|Opaque)\s', i.strip()) is None]
+    if len(sans_opacity) == 1:
+        return sans_opacity
+    else:
+        return statements
+
 def move_from_proof(filename, **kwargs):
     if kwargs['verbose']: kwargs['log']('Processing %s...' % filename)
     try:
@@ -106,6 +151,7 @@ def move_from_proof(filename, **kwargs):
     ret = []
     cur_statements = []
     cur_statement = []
+    deferred_copied_statements = []
     deferred_statements = []
     orig_space_count = 0
     cur_diff_space_count = 0
@@ -138,30 +184,37 @@ def move_from_proof(filename, **kwargs):
         elif is_definition_end and cur_statement:
             if kwargs['verbose'] >= 2: kwargs['log']('Ending definition: ' + repr(i))
             cur_statement.append(remove_leading_space(i, cur_diff_space_count))
-            cur_statements.extend(cur_statement)
+            cur_statements.append(''.join(cur_statement))
+            #print(''.join(preminimize_lifted_statements(cur_statements)))
             if deferred_statements:
                 cur_diff_space_count, cur_statement = deferred_statements.pop()
+                deferred_copied_statements = list(preminimize_lifted_statements(deferred_copied_statements))
+                cur_statement.extend(deferred_copied_statements) # todo: fix indentation
             else:
-                ret.extend(cur_statements)
+                #print('extending')
+                #print(''.join(minimize_lifted_statements(cur_statements)))
+                ret.extend(list(minimize_lifted_statements(cur_statements)))
                 cur_statement = []
                 cur_statements = []
+                deferred_copied_statements = []
         elif MOVE_UP_REG.match(i) or is_definition_full:
             if kwargs['verbose'] >= 2: kwargs['log']('Lifting: ' + repr(i))
             cur_statements.append(set_leading_space(i, orig_space_count))
         elif COPY_UP_REG.match(i) and cur_statement:
-            if kwargs['verbose'] >= 3: kwargs['log'](repr(i))
+            if kwargs['verbose'] >= 2: kwargs['log']('Lift-copying: ' + repr(i))
             cur_statement.append(remove_leading_space(i, cur_diff_space_count))
             cur_statements.append(set_leading_space(i, orig_space_count))
+            deferred_copied_statements.append(remove_leading_space(i, cur_diff_space_count))
         else:
             raw_input('WARNING: Unrecognized: %s' % repr(i))
         #print(cur_diff_space_count)
         #print(remove_leading_space(i, cur_diff_space_count))
     if cur_statements or deferred_statements or cur_statement:
         raw_input('WARNING: extra statements: %s' % repr((cur_statements, cur_statement, deferred_statements)))
-        cur_statements.extend(cur_statement)
+        cur_statements.append(''.join(cur_statement))
         for i in deferred_statements:
-            cur_statements.extend(i)
-        ret.extend(cur_statements)
+            cur_statements.append(''.join(i))
+        ret.extend(list(minimize_lifted_statements(cur_statements)))
     ret = ''.join(ret)
     if ret == contents:
         return
