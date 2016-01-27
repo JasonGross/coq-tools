65e753f8159b0285963ea0dc7a16215a54978530
Jason Gross <jgross@mit.edu>
2016-01-27 01:17:18 -0500

    Fix example 9 test

--- examples/run-example-9.sh
+++ examples/run-example-9.sh
@@ -17,7 +17,7 @@ then
     exit 1
 fi
 python ../../find-bug.py example_9.v bug_9.v || exit $?
-EXPECTED='^(\* File reduced by coq-bug-finder from original input, then from [0-9]+ lines to [0-9]+ lines, then from [0-9]+ lines to [0-9]+ lines \*)$'
+EXPECTED='^(\* File reduced by coq-bug-finder from original input, then from [0-9]\+ lines to [0-9]\+ lines, then from [0-9]\+ lines to [0-9]\+ lines \*)$'
 LINES="$(grep -c "$EXPECTED" bug_9.v)"
 if [ "$LINES" -ne 1 ]
 then
