002c798616a1197cbbad9477fd06ce4883cf1d0c
Jason Gross <jgross@mit.edu>
2016-06-02 15:02:26 -0400

    Remove use of --directory

--- examples/run-example-01.sh
+++ examples/run-example-01.sh
@@ -4,5 +4,4 @@ cd "$DIR"
 cd "example_01"
 PS4='$ '
 set -x
-# Use absolute paths because --directory messes with relative paths
 python ../../find-bug.py C.v ../example_01_output.v --no-minimize-before-inlining "$@" -l - ../example_01_log.log || exit $?
