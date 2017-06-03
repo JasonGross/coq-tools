fd7109cad0083f6432a14b8a4dd95499812f410e
Jason Gross <jgross@mit.edu>
2017-06-03 17:00:51 -0400

    Fix example_08-3 for make -B

--- examples/run-example-08-3.sh
+++ examples/run-example-08-3.sh
@@ -3,6 +3,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/example_08"
 PS4='$ '
 set -x
+# account for a super-make called with -B
+export MAKEFLAGS="" # B -- VERBOSE=1 V=1
+export MFLAGS="" # -B
 python ../../find-bug.py example_08.v bug_08_3.v --coqc-is-coqtop "$@" || exit $?
 LINES="$(cat bug_08_3.v | grep -v '^$' | wc -l)"
 if [ "$LINES" -ne 10 ]
