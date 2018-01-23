a7637797af0893d5e1d8b02a18d206e145d9ed4a
Jason Gross <jgross@mit.edu>
2018-01-23 13:36:43 -0500

    [travis] Do not fail on W: ... Signature by key ... uses weak digest algorithm (SHA1)

--- etc/sudo-apt-get-update.sh
+++ etc/sudo-apt-get-update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 (sudo apt-get update "$@" 2>&1 || echo 'E: update failed') | tee /tmp/apt.err
-! grep -q '^[WE]' /tmp/apt.err || exit $?
+! grep -q '^\(E:\|W: Failed to fetch\)' /tmp/apt.err || exit $?
