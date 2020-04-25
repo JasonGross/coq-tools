b0335076b0bcd57bfd5fb7a5420a502d7aa55745
Jason Gross <jgross@mit.edu>
2020-04-24 20:03:57 -0400

    Fix example 8

--- examples/run-example-08.sh
+++ examples/run-example-08.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
+. "$DIR/init-settings.sh"
 PS4='$ '
 set -x
 # Disable parallel make in subcalls to the bug minimizer because it screws with things
