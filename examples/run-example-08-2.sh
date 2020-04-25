aedf672de590eee61ddeb8216071a530b0282ed8
Jason Gross <jgross@mit.edu>
2020-04-25 16:17:51 -0400

    Better cleaning

--- examples/run-example-08-2.sh
+++ examples/run-example-08-2.sh
@@ -6,7 +6,7 @@ PS4='$ '
 set -x
 # Disable parallel make in subcalls to the bug minimizer because it screws with things
 . "$DIR/disable-parallel-make.sh"
-rm -f *.vo *.glob
+rm -f *.vo *.glob *.d .*.d
 ${PYTHON} ../../find-bug.py example_08.v bug_08_2.v || exit $?
 LINES="$(cat bug_08_2.v | grep -v '^$' | wc -l)"
 if [ "$LINES" -ne 10 ]
