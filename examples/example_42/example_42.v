679ec33887ca2849eefc8a652be53a208776d9d5
Jason Gross <jgross@mit.edu>
2021-06-19 23:31:10 -0400

    Add a comment to future-proof things a bit more

--- examples/example_42/example_42.v
+++ examples/example_42/example_42.v
@@ -1,4 +1,8 @@
 Require Import Foo.B Foo.C.
+(* N.B. Consider replacing this with the example of
+https://github.com/JasonGross/coq-tools/issues/16, since the issue
+with [congruence] may go away, and all we really want to test here is
+that the python code for un-inlinable modules runs fine *)
 
 Definition bar := Eval unfold foo in foo.
 
