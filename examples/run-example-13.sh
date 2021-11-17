2fde508408d6ea5b20bf54c0097f060a7040302f
Jason Gross <jgross@mit.edu>
2021-11-17 11:05:01 -0800

    Smaller example 13 test

--- examples/run-example-13.sh
+++ examples/run-example-13.sh
@@ -42,15 +42,8 @@ EXPECTED_ERROR=$(cat <<EOF
 This file produces the following output when Coq'ed:
 File "/tmp/tmp[A-Za-z0-9_/]\+\.v", line 1[0-9], characters \(6-9\|0-16\):
 Error:
-The term "foo" has type "Type" while it is expected to have type\s\?
-"Set" (universe inconsistency[^)]*)\.
-
-.\?Does this output display the correct error? \[(y)es/(n)o\]\s
-I think the error is 'Error:
-The term "foo" has type "Type" while it is expected to have type\s\?
-"Set" (universe inconsistency[^)]*)\.
-.\?'\.
-The corresponding regular expression is 'File "\[^"\]+", line (\[0-9\]+), characters \[0-9-\]+:\\\\n(Error:\\\\sThe\\\\s+term\\\\s+"foo"\\\\s+has\\\\s+type\\\\s+"Type"\\\\s+while\\\\s+it\\\\s+is\\\\s+expected\\\\s+to\\\\s+have\\\\s+type\\\\s+"Set"\\\\s+\\\\(universe\\\\s+inconsistency[^)]*\\\\)\\\\.*
+The term "foo" has type "Type" while it is expected to have type[
+ ]\+"Set".*
 EOF
 )
 # pre-build the files to normalize the output for the run we're testing
