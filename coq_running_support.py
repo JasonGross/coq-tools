68372cdaad30ece7a3ef3eda4212ebe59db93e2c
Enrico Tassi <Enrico.Tassi@Inria.fr>
2021-12-09 10:03:01 +0100

    adapt to coq/coq#15220

--- coq_running_support.py
+++ coq_running_support.py
@@ -24,8 +24,8 @@ Tactic Notation "admit" := abstract case proof_admitted.
 Goal False. admit. Qed.'''
     errinfo = {}
     native_ondemand_args = list(get_coq_native_compiler_ondemand_fragment(coqc, **kwargs))
-    for before, after in (('Declare ML Module "ltac_plugin".\n', ''),
-                          ('Declare ML Module "ltac_plugin".\n', 'Global Set Default Proof Mode "Classic".\n'),
+    for before, after in (('Declare ML Module "coq-core.plugins.ltac".\n', ''),
+                          ('Declare ML Module "coq-core.plugins.ltac".\n', 'Global Set Default Proof Mode "Classic".\n'),
                           ('Require Coq.Init.Ltac.\n', 'Import Coq.Init.Ltac.\n'),
                           ('Require Coq.Init.Notations.\n', 'Import Coq.Init.Notations.\n')):
         contents = '%s\n%s\n%s' % (before, after, test)
