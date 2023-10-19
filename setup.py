2a31b772a10cf2cd840ce26c55a0b19dea3cca64
Jason Gross <jgross@mit.edu>
2023-10-19 16:31:27 -0700

    Fix inclusion of extra scripts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='coq-tools',
-    version='0.1',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
         # Add any dependencies here
@@ -10,6 +10,12 @@ setup(
     scripts=[
         # Add non-python executable scripts to install here
     ],
+    package_data={
+        'coq_tools': [
+            'coqtop-as-coqc.sh',
+            'coqtop.bat',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'coq-bug-minimizer=coq_tools.find_bug:main',
