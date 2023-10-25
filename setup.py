f6da4c3b9ae131db517167a49cbbd0705043490f
JasonGross <JasonGross@users.noreply.github.com>
2023-10-25 06:31:02 +0000

    [create-pull-request] automated change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='coq-tools',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[
         # Add any dependencies here
