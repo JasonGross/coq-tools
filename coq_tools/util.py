6392e560ba6c652fae9bad6418025020f35873c3
Jason Gross <jgross@mit.edu>
2024-03-06 15:20:05 -0800

    Run black on util

--- coq_tools/util.py
+++ coq_tools/util.py
@@ -1,27 +1,51 @@
 import os, sys, re
 
-__all__ = ["prompt", "yes_no_prompt", "b", "s", "cmp_compat", "PY3", "raw_input", "re_escape", "slice_string_at_bytes", "len_in_bytes", "shlex_quote", "resource_path"]
+__all__ = [
+    "prompt",
+    "yes_no_prompt",
+    "b",
+    "s",
+    "cmp_compat",
+    "PY3",
+    "raw_input",
+    "re_escape",
+    "slice_string_at_bytes",
+    "len_in_bytes",
+    "shlex_quote",
+    "resource_path",
+]
 
 SCRIPT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 if sys.version_info < (3,):
     PY3 = False
+
     def b(x):
         return x
+
     def s(x):
         return x
+
 else:
     PY3 = True
+
     def b(x):
-        if x is not None: return x.encode()
+        if x is not None:
+            return x.encode()
+
     def s(x):
         # Sometimes we get str rather than bytes??? cf https://gitlab.com/coq/coq/-/jobs/544269051
-        if hasattr(x, 'decode'): return x.decode('utf-8', 'ignore')
+        if hasattr(x, "decode"):
+            return x.decode("utf-8", "ignore")
         return x
+
     def cmp(x, y):
-        if x < y: return -1
-        if y < x: return 1
+        if x < y:
+            return -1
+        if y < x:
+            return 1
         return 0
+
     raw_input = input
 
 if sys.version_info < (3, 7):
@@ -29,15 +53,26 @@ if sys.version_info < (3, 7):
     def re_escape(*args, **kwargs):
         ret = re.escape(*args, **kwargs)
         for ch in ':"':
-            ret = ret.replace('\\' + ch, ch)
+            ret = ret.replace("\\" + ch, ch)
         return ret
+
 else:
     re_escape = re.escape
 
 cmp_compat = cmp
 
-def prompt(options, case_sensitive=False, strip=True, sep='/', prefix='Please enter ', postfix=': ', yes_value=True, yes=False):
-    msg = prefix + sep.join(i['display'] for i in options) + postfix
+
+def prompt(
+    options,
+    case_sensitive=False,
+    strip=True,
+    sep="/",
+    prefix="Please enter ",
+    postfix=": ",
+    yes_value=True,
+    yes=False,
+):
+    msg = prefix + sep.join(i["display"] for i in options) + postfix
     if yes:
         print(msg)
         return yes_value
@@ -49,16 +84,28 @@ def prompt(options, case_sensitive=False, strip=True, sep='/', prefix='Please en
             if strip:
                 response = response.strip()
             for expected in options:
-                if response in expected['values']:
-                    return expected['value']
-            print('Response "%s" is not one of %s' % (response, ', '.join(repr(val) for i in options for val in i['values'])))
+                if response in expected["values"]:
+                    return expected["value"]
+            print(
+                'Response "%s" is not one of %s'
+                % (
+                    response,
+                    ", ".join(repr(val) for i in options for val in i["values"]),
+                )
+            )
             response = raw_input(msg)
 
+
 def yes_no_prompt(**kwargs):
-    return prompt(({'value':True, 'display':'(y)es', 'values':('y', 'yes')},
-                   {'value':False, 'display':'(n)o', 'values':('n', 'no')}),
-                  yes_value=True,
-                  **kwargs)
+    return prompt(
+        (
+            {"value": True, "display": "(y)es", "values": ("y", "yes")},
+            {"value": False, "display": "(n)o", "values": ("n", "no")},
+        ),
+        yes_value=True,
+        **kwargs,
+    )
+
 
 # Unfortunately, coqtop -emacs -time reports character locations in
 # bytes, as does the glob file, so we need to handle unicode.  Here we
@@ -66,47 +113,57 @@ def yes_no_prompt(**kwargs):
 # versions of python
 def slice_string_at_bytes(string, start=None, end=None):
     string = b(string)
-    if start is None: start = 0
-    if end is None: end = len(string)
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(string)
     return s(string[start:end])
+
 
 def len_in_bytes(string):
     return len(b(string))
 
+
 def normalize_newlines(string):
-    return string.replace('\r\n', '\n').replace('\r', '\n')
+    return string.replace("\r\n", "\n").replace("\r", "\n")
+
 
 # Terminal colors (maybe something cleverer needs to be done for other
 # platforms).
 class colors:
-  ESC ='\033'
-  #Escape code doesn't render on github so we use the standard escaped escape.
-  #ESC = ''
+    ESC = "\033"
+    # Escape code doesn't render on github so we use the standard escaped escape.
+    # ESC = ''
 
-  HEADER    = ESC + '[95m'
-  OKBLUE    = ESC + '[94m'
-  OKCYAN    = ESC + '[96m'
-  OKGREEN   = ESC + '[92m'
-  WARNING   = ESC + '[93m'
-  FAIL      = ESC + '[91m'
-  ENDC      = ESC + '[0m'
-  BOLD      = ESC + '[1m'
-  UNDERLINE = ESC + '[4m'
+    HEADER = ESC + "[95m"
+    OKBLUE = ESC + "[94m"
+    OKCYAN = ESC + "[96m"
+    OKGREEN = ESC + "[92m"
+    WARNING = ESC + "[93m"
+    FAIL = ESC + "[91m"
+    ENDC = ESC + "[0m"
+    BOLD = ESC + "[1m"
+    UNDERLINE = ESC + "[4m"
+
 
 # Colors a string a given color
 # Example usage: color("Hello World!", colors.OKBLUE)
 def color(str, col, on=True):
     if not on:
-      return str;
+        return str
     else:
-      return col + str + colors.ENDC;
+        return col + str + colors.ENDC
+
 
 if sys.version_info < (3, 3):
     import pipes
+
     shlex_quote = pipes.quote
 else:
     import shlex
+
     shlex_quote = shlex.quote
+
 
 def resource_path(path):
     if os.path.exists(os.path.join(SCRIPT_DIRECTORY, path)):
@@ -114,8 +171,8 @@ def resource_path(path):
     if sys.version_info < (3, 7):
         import pkg_resources
 
-        return pkg_resources.resource_filename('coq_tools', path)
+        return pkg_resources.resource_filename("coq_tools", path)
     else:
         import importlib.resources
 
-        return importlib.resources.path('coq_tools', path)
+        return importlib.resources.path("coq_tools", path)
