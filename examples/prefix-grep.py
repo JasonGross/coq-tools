a2e174a9898b28c4d0225c8b6e0d5717f444d69c
Jason Gross <jgross@mit.edu>
2024-09-22 14:00:50 -0700

    Autoreformat with vscode

--- examples/prefix-grep.py
+++ examples/prefix-grep.py
@@ -8,15 +8,21 @@ INVALID = None
 
 # http://python3porting.com/problems.html
 if sys.version_info < (3,):
+
     def b(x):
         return x
+
     def s(x):
         return x
+
 else:
+
     def b(x):
         return x.encode()
+
     def s(x):
         return x.decode()
+
 
 def binary_search_on_string(f, arg):
     start = 0
@@ -58,7 +64,7 @@ def binary_search_on_string(f, arg):
                         sys.stderr.write("This should be impossible\n")
                         end = mid
                         break
-    while end + 1 < len(arg) and f(arg[start:end + 1]) == GOOD:
+    while end + 1 < len(arg) and f(arg[start : end + 1]) == GOOD:
         sys.stderr.write("This should be impossible (2)\n")
         end += 1
     orig_end = end
@@ -68,24 +74,34 @@ def binary_search_on_string(f, arg):
             orig_end = end
     return arg[start:orig_end]
 
+
 @memoize
 def check_grep_for(in_str, search_for):
     # print("echo %s | grep -q %s" % (repr(in_str), repr(search_for)))
-    p = subprocess.Popen(["timeout", "0.5s", "grep", search_for], universal_newlines=False, stderr=subprocess.PIPE, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    p = subprocess.Popen(
+        ["timeout", "0.5s", "grep", search_for],
+        universal_newlines=False,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+    )
     # print(search_for)
     (stdout, stderr) = p.communicate(input=b(in_str))
     p.stdin.close()
     p.wait()
-    if stderr != b('') or p.returncode == 124: # timeout
+    if stderr != b("") or p.returncode == 124:  # timeout
         return INVALID
-    return (GOOD if p.returncode == 0 else BAD)
+    return GOOD if p.returncode == 0 else BAD
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) != 3:
         sys.stderr.write("USAGE: %s SEARCH_IN SEARCH_FOR\n" % sys.argv[0])
         sys.exit(1)
+
     def check_grep(search_for):
         return check_grep_for(sys.argv[1], search_for)
+
     search_for = binary_search_on_string(check_grep, sys.argv[2])
     p = subprocess.Popen(["grep", "--color=auto", search_for], universal_newlines=False, stdin=subprocess.PIPE)
     p.communicate(input=b(sys.argv[1]))
@@ -93,15 +109,28 @@ if __name__ == '__main__':
     p.wait()
     if len(search_for) < len(sys.argv[2]):
         print("Mismatch: good prefix:")
-        p = subprocess.Popen(["grep", "-o", "--color=auto", search_for], universal_newlines=False, stdin=subprocess.PIPE)
+        p = subprocess.Popen(
+            ["grep", "-o", "--color=auto", search_for], universal_newlines=False, stdin=subprocess.PIPE
+        )
         p.communicate(input=b(sys.argv[1]))
         p.stdin.close()
         p.wait()
-        p = subprocess.Popen(["grep", "-o", '^.*' + search_for], universal_newlines=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(
+            ["grep", "-o", "^.*" + search_for],
+            universal_newlines=False,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
         (stdout, stderr) = p.communicate(input=b(sys.argv[1]))
         p.stdin.close()
         p.wait()
         print("Mismatch: good prefix search:\n%s" % search_for)
-        print("Mismatch: expected next characters at %d: %s" % (len(search_for), repr(sys.argv[2][len(search_for):][:10])))
-        print("Mismatch: actual next characters at %d: %s" % (len(stdout)-1, repr(sys.argv[1][len(stdout)-1:][:10])))
-    #sys.exit(p.errorcode)
+        print(
+            "Mismatch: expected next characters at %d: %s"
+            % (len(search_for), repr(sys.argv[2][len(search_for) :][:10]))
+        )
+        print(
+            "Mismatch: actual next characters at %d: %s" % (len(stdout) - 1, repr(sys.argv[1][len(stdout) - 1 :][:10]))
+        )
+    # sys.exit(p.errorcode)
