b58b18105a37469ef7f36099af037810541f32a9
Jason Gross <jgross@mit.edu>
2024-10-24 19:06:54 -0700

    Be more Mac OSX proof for example 027

--- examples/run-example-027.sh
+++ examples/run-example-027.sh
@@ -38,7 +38,7 @@ set -x
 # free to remove this section.
 #
 # Note that the -top argument only appears in Coq >= 8.4
-EXPECTED_ERROR=$(cat <<EOF
+{ EXPECTED_ERROR=$(cat); } <<EOF
 File "/[a-z]\+/tmp[A-Za-z0-9_/]\+\.v", line 1[0-9], characters 6-15:
 Error:
 The term "A.foo" has type "Type" while it is expected to have type\s\?
@@ -50,7 +50,6 @@ The term "A.foo" has type "Type" while it is expected to have type\s\?
 "Set" (universe inconsistency[^)]*)\.
 .\?'\.
 EOF
-)
 # pre-build the files to normalize the output for the run we're testing
 rm -f *.vo *.glob *.d .*.d
 echo "y" | find_bug "$EXAMPLE_INPUT" "$EXAMPLE_OUTPUT" -R . Foo # 2>/dev/null >/dev/null
@@ -83,7 +82,7 @@ find_bug "$EXAMPLE_INPUT" "$EXAMPLE_OUTPUT" -R . Foo || exit $?
 # Put some segment that you expect to see in the file here.  Or count
 # the number of lines.  Or make some other test.  Or remove this block
 # entirely if you don't care about the minimized file.
-EXPECTED=$(cat <<EOF
+{ EXPECTED=$(cat); } <<EOF
 (\* -\*- mode: coq; coq-prog-args: ([^)]*) -\*- \*)
 (\* File reduced by coq-bug-minimizer from original input, then from [0-9]\+ lines to [0-9]\+ lines, then from [0-9]\+ lines to [0-9]\+ lines, then from [0-9]\+ lines to [0-9]\+ lines, then from [0-9]\+ lines to [0-9]\+ lines \*)
 (\* coqc version [^\*]*\*)
@@ -101,7 +100,6 @@ End A\.
 \?Check Foo\.A\.foo : Set\.
 
 EOF
-)
 
 EXPECTED_ONE_LINE="$(echo "$EXPECTED" | grep -v '^$' | tr '\n' '\1')"
 ACTUAL="$(cat "$EXAMPLE_OUTPUT" | grep -v '^$' | tr '\n' '\1')"
