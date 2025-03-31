649762096f6a827c034afc13fd40090fc20a868e
Jason Gross <jgross@mit.edu>
2025-03-31 13:54:13 -0700

    Use `Print Options` for better minimization control
    Fixes #113

--- coq_tools/coq_running_support.py
+++ coq_tools/coq_running_support.py
@@ -8,7 +8,12 @@ from .coq_full_grammar import COQ_GRAMMAR_TOKENS
 # perhaps a bit more generally for things that need to run coqc on a
 # file)
 
-__all__ = ["get_proof_term_works_with_time", "get_ltac_support_snippet", "get_reserved_modnames"]
+__all__ = [
+    "get_proof_term_works_with_time",
+    "get_ltac_support_snippet",
+    "get_reserved_modnames",
+    "get_default_options_settings",
+]
 
 
 def get_proof_term_works_with_time(coqc_prog, **kwargs):
@@ -106,3 +111,64 @@ def get_reserved_modnames(coqtop_prog, **kwargs):
     )
     success = re.findall(r"Module ([^ ]+) is defined", output)
     return tuple(modname for modname in tokens if modname not in success)
+
+
+def filter_check_options_after_success(coqc_prog, contents: str, options: list, **kwargs):
+    if not options:
+        return options
+    options_contents = contents + "\n" + "\n".join(options)
+    output, cmds, retcode, runtime = get_coq_output(
+        coqc_prog,
+        ("-q", *kwargs.get("coqc_args", ())),
+        options_contents,
+        None,
+        **kwargs,
+    )
+    if retcode == 0:
+        return options
+    elif len(options) == 1:
+        return []
+    options_1 = options[: len(options) // 2]
+    options_2 = options[len(options) // 2 :]
+    return filter_check_options_after_success(
+        coqc_prog, contents, options_1, **kwargs
+    ) + filter_check_options_after_success(coqc_prog, contents, options_2, **kwargs)
+
+
+def get_default_options_settings(
+    coqc_prog, after_contents: str = "", prefix: str = "", coqc_args=(), coqc_is_coqtop=False, **kwargs
+):
+    options_contents = f"{after_contents}\nPrint Options.\n"
+    skip_args = {"-vos", "-vok"}
+    coqc_args = tuple([arg for arg in coqc_args if arg not in skip_args])
+    output, cmds, retcode, runtime = get_coq_output(
+        coqc_prog,
+        coqc_args,
+        options_contents,
+        None,
+        is_coqtop=coqc_is_coqtop,
+        **kwargs,
+    )
+    output = f"\n{output}"
+    # compat with Coq 8.4, 8.5, 8.6, 8.7
+    output = output.replace("\nSynchronous options:", "\nOptions:").replace("\nAsynchronous options:", "")
+    assert "\nOptions:" in output, output
+    _, options_str = output.split("\nOptions:", maxsplit=1)
+    options_str, _tables_str = options_str.split("\nTables:", maxsplit=1)
+    options = [
+        opt.strip() for opt in options_str.split("\n") if opt.strip() and not opt.strip().startswith("[DEPRECATED")
+    ]
+    options = [opt[: -len("[DEPRECATED]")].strip() for opt in options if opt.endswith("[DEPRECATED]")]
+    options_settings = [opt.split(": ", maxsplit=1) for opt in options]
+    commands = []
+    for opt_val in options_settings:
+        if len(opt_val) == 2:
+            opt, val = opt_val
+            if val == "undefined" or val == "off":
+                commands.append(f"{prefix}Unset {opt}.")
+            elif val == "on":
+                commands.append(f"{prefix}Set {opt}.")
+            else:
+                commands.append(f"{prefix}Set {opt} {val}.")
+
+    return "\n".join(filter_check_options_after_success(coqc_prog, after_contents, commands, **kwargs))
