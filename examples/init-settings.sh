0fbacb6aaca8380bb34c3cd9f8c07361a44f4a47
Jason Gross <jgross@mit.edu>
2025-07-15 13:11:31 -0700

    Export GREP and grep_contains in init-settings for better grep support (upcoming)

--- examples/init-settings.sh
+++ examples/init-settings.sh
@@ -15,6 +15,15 @@ if [ -z "${PYTHON}" ]; then
     export PYTHON
 fi
 
+if [ -z "${GREP}" ]; then
+    if command -v ggrep >/dev/null 2>&1; then
+        GREP="ggrep"
+    else
+        GREP="grep"
+    fi
+    export GREP
+fi
+
 if [ -z "${FIND_BUG}" ]; then
     function find_bug() {
         ${PYTHON} "${FIND_BUG_PY}" "$@"
@@ -87,7 +96,7 @@ fi
 export -f relpath
 
 function strip_for_grep() {
-    s="$(printf "%s" "$1" | grep -v '^$' | tr -d '\r')"
+    s="$(printf "%s" "$1" | "$GREP" -v '^$' | tr -d '\r')"
     # Trim leading whitespace
     s="${s#"${s%%[![:space:]]*}"}"
     # Trim trailing whitespace
@@ -97,3 +106,16 @@ function strip_for_grep() {
 }
 
 export -f strip_for_grep
+
+function grep_contains() {
+    count="$(printf '%s' "$1" | "$GREP" -c "$2")"
+    if [ -z "$count" ]; then
+        return 1
+    elif [ "$count" -lt 1 ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+export -f grep_contains
