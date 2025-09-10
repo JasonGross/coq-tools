aaa46365daed6364f91a299487f888f7db105351
Jason Gross <jgross@mit.edu>
2025-09-10 14:24:05 -0700

    Add convenience get_raw_options_settings_and_values

--- coq_tools/coq_running_support.py
+++ coq_tools/coq_running_support.py
@@ -1,7 +1,11 @@
 from __future__ import with_statement
 import re
 from .diagnose_error import get_coq_output
-from .coq_version import get_coq_native_compiler_ondemand_fragment, get_boot_noinputstate_args, coqlib_args_of_coq_args
+from .coq_version import (
+    get_coq_native_compiler_ondemand_fragment,
+    get_boot_noinputstate_args,
+    coqlib_args_of_coq_args,
+)
 from .coq_full_grammar import COQ_GRAMMAR_TOKENS
 
 # Like coq_version.py, except for things that use get_coq_output (or
@@ -13,6 +17,7 @@ __all__ = [
     "get_ltac_support_snippet",
     "get_reserved_modnames",
     "get_default_options_settings",
+    "get_raw_options_settings_and_values",
 ]
 
 
@@ -41,21 +46,32 @@ Axiom proof_admitted : False.
 Tactic Notation "admit" := abstract case proof_admitted.
 Goal False. admit. Qed."""
     errinfo = {}
-    native_ondemand_args = list(get_coq_native_compiler_ondemand_fragment(coqc, coq_args=coqc_args, **kwargs))
+    native_ondemand_args = list(
+        get_coq_native_compiler_ondemand_fragment(coqc, coq_args=coqc_args, **kwargs)
+    )
     for before, after in (
         ("Require Coq.Init.Ltac.\n", "Import Coq.Init.Ltac.\n"),
         ("Require Coq.Init.Notations.\n", "Import Coq.Init.Notations.\n"),
         ("Require Stdlib.Init.Ltac.\n", "Import Stdlib.Init.Ltac.\n"),
         ("Require Stdlib.Init.Notations.\n", "Import Stdlib.Init.Notations.\n"),
         ('Declare ML Module "coq-core.plugins.ltac".\n', ""),
-        ('Declare ML Module "coq-core.plugins.ltac".\n', 'Global Set Default Proof Mode "Classic".\n'),
+        (
+            'Declare ML Module "coq-core.plugins.ltac".\n',
+            'Global Set Default Proof Mode "Classic".\n',
+        ),
         ('Declare ML Module "ltac_plugin".\n', ""),
-        ('Declare ML Module "ltac_plugin".\n', 'Global Set Default Proof Mode "Classic".\n'),
+        (
+            'Declare ML Module "ltac_plugin".\n',
+            'Global Set Default Proof Mode "Classic".\n',
+        ),
     ):
         contents = "%s\n%s\n%s" % (before, after, test)
         output, cmds, retcode, runtime = get_coq_output(
             coqc,
-            tuple(["-q", "-nois", *coqlib_args_of_coq_args(coqc, coqc_args, **kwargs)] + native_ondemand_args),
+            tuple(
+                ["-q", "-nois", *coqlib_args_of_coq_args(coqc, coqc_args, **kwargs)]
+                + native_ondemand_args
+            ),
             contents,
             timeout_val=None,
             verbose_base=3,
@@ -66,8 +82,15 @@ Goal False. admit. Qed."""
             LTAC_SUPPORT_SNIPPET[coqc] = (before, after)
             return (before, after)
         else:
-            errinfo[contents] = {"output": output, "cmds": cmds, "retcode": retcode, "runtime": runtime}
-    raise Exception("No valid ltac support snipped found.  Debugging info: %s" % repr(errinfo))
+            errinfo[contents] = {
+                "output": output,
+                "cmds": cmds,
+                "retcode": retcode,
+                "runtime": runtime,
+            }
+    raise Exception(
+        "No valid ltac support snipped found.  Debugging info: %s" % repr(errinfo)
+    )
 
 
 def get_is_modname_valid(coqc_prog, modname, **kwargs):
@@ -86,7 +109,9 @@ def get_is_modname_valid(coqc_prog, modname, **kwargs):
 
 
 def get_reserved_modnames(coqtop_prog, **kwargs):
-    grammars_contents = "Print Grammar tactic. Print Grammar constr. Print Grammar vernac."
+    grammars_contents = (
+        "Print Grammar tactic. Print Grammar constr. Print Grammar vernac."
+    )
     grammars_output, cmds, retcode, runtime = get_coq_output(
         coqtop_prog,
         ("-q", *get_boot_noinputstate_args(coqtop_prog, **kwargs)),
@@ -97,8 +122,15 @@ def get_reserved_modnames(coqtop_prog, **kwargs):
         verbose_base=3,
         **kwargs,
     )
-    tokens = sorted(set([i.strip('"') for i in re.findall(r'"[a-zA-Z_][^"]*"', grammars_output)] + COQ_GRAMMAR_TOKENS))
-    contents = "\n".join("Module %s. End %s." % (modname, modname) for modname in tokens)
+    tokens = sorted(
+        set(
+            [i.strip('"') for i in re.findall(r'"[a-zA-Z_][^"]*"', grammars_output)]
+            + COQ_GRAMMAR_TOKENS
+        )
+    )
+    contents = "\n".join(
+        "Module %s. End %s." % (modname, modname) for modname in tokens
+    )
     output, cmds, retcode, runtime = get_coq_output(
         coqtop_prog,
         ("-q", *get_boot_noinputstate_args(coqtop_prog, **kwargs)),
@@ -113,7 +145,9 @@ def get_reserved_modnames(coqtop_prog, **kwargs):
     return tuple(modname for modname in tokens if modname not in success)
 
 
-def filter_check_options_after_success(coqc_prog, contents: str, options: list, **kwargs):
+def filter_check_options_after_success(
+    coqc_prog, contents: str, options: list, **kwargs
+):
     if not options:
         return options
     options_contents = contents + "\n" + "\n".join(options)
@@ -135,8 +169,8 @@ def filter_check_options_after_success(coqc_prog, contents: str, options: list, 
     ) + filter_check_options_after_success(coqc_prog, contents, options_2, **kwargs)
 
 
-def get_default_options_settings(
-    coqc_prog, after_contents: str = "", prefix: str = "", coqc_args=(), coqc_is_coqtop=False, **kwargs
+def get_raw_options_settings_and_values(
+    coqc_prog, after_contents: str = "", coqc_args=(), coqc_is_coqtop=False, **kwargs
 ):
     options_contents = f"{after_contents}\nPrint Options.\n"
     skip_args = {"-vos", "-vok"}
@@ -151,15 +185,42 @@ def get_default_options_settings(
     )
     output = f"\n{output}"
     # compat with Coq 8.4, 8.5, 8.6, 8.7
-    output = output.replace("\nSynchronous options:", "\nOptions:").replace("\nAsynchronous options:", "")
+    output = output.replace("\nSynchronous options:", "\nOptions:").replace(
+        "\nAsynchronous options:", ""
+    )
     assert "\nOptions:" in output, output
     _, options_str = output.split("\nOptions:", maxsplit=1)
     options_str, _tables_str = options_str.split("\nTables:", maxsplit=1)
     options = [
-        opt.strip() for opt in options_str.split("\n") if opt.strip() and not opt.strip().startswith("[DEPRECATED")
+        opt.strip()
+        for opt in options_str.split("\n")
+        if opt.strip() and not opt.strip().startswith("[DEPRECATED")
     ]
-    options = [opt[: -len("[DEPRECATED]")].strip() for opt in options if opt.endswith("[DEPRECATED]")]
+    options = [
+        opt[: -len("[DEPRECATED]")].strip()
+        for opt in options
+        if opt.endswith("[DEPRECATED]")
+    ]
     options_settings = [opt.split(": ", maxsplit=1) for opt in options]
+    return options_settings
+
+
+def get_default_options_settings(
+    coqc_prog,
+    after_contents: str = "",
+    prefix: str = "",
+    coqc_args=(),
+    coqc_is_coqtop=False,
+    **kwargs,
+):
+    options_settings = get_raw_options_settings_and_values(
+        coqc_prog,
+        after_contents,
+        prefix,
+        coqc_args=coqc_args,
+        coqc_is_coqtop=coqc_is_coqtop,
+        **kwargs,
+    )
     commands = []
     for opt_val in options_settings:
         if len(opt_val) == 2:
@@ -171,4 +232,8 @@ def get_default_options_settings(
             else:
                 commands.append(f"{prefix}Set {opt} {val}.")
 
-    return "\n".join(filter_check_options_after_success(coqc_prog, after_contents, commands, **kwargs))
+    return "\n".join(
+        filter_check_options_after_success(
+            coqc_prog, after_contents, commands, **kwargs
+        )
+    )
