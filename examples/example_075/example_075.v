90c0f91aa3551b0862d451f356bae21b35f28d48
Jason Gross <jgross@mit.edu>
2025-11-23 11:07:36 -0800

    Adjust example 075

--- examples/example_075/example_075.v
+++ examples/example_075/example_075.v
@@ -1,6 +1,20 @@
 Require Import ZArith.
+Require Import List.
+Module List.
+    Section Repeat.
+        Variable A : Type.
+        Fixpoint repeat (x : A) (n: nat ) :=
+            match n with
+            | O => nil
+            | S k => cons x (repeat x k)
+            end.
+
+    End Repeat.
+End List.
 Notation hide := _.
 Check I.
+Fail I.
+Goal True. idtac "File ""./example_075.v"", line 999, characters 0-0:". idtac "Error:". Abort.
 Ltac big n :=
     match n with
     | O => idtac
