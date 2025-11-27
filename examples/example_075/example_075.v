6a93de3e3406d1106cbeee5bf543d5ee0c9468e5
Jason Gross <jgross@mit.edu>
2025-11-27 13:42:45 -0800

    Adjust example to allocate more mem

--- examples/example_075/example_075.v
+++ examples/example_075/example_075.v
@@ -1,26 +1,10 @@
-Require Import ZArith.
-Require Import List.
-Module List.
-    Section Repeat.
-        Context {A : Type}.
-        Fixpoint repeat (x : A) (n: nat ) :=
-            match n with
-            | O => nil
-            | S k => cons x (repeat x k)
-            end.
-
-    End Repeat.
-End List.
+Require Import Coq.Lists.List.
+Fixpoint seq (start len : nat) : list nat :=
+  match len with
+  | 0 => nil
+  | S len0 => cons start (seq (S start) len0)
+  end.
 Notation hide := _.
 Check I.
-Ltac big n :=
-    match n with
-    | O => idtac
-    | S ?n => let x := eval vm_compute in (List.repeat Z.div_eucl 16) in
-        big n
-    end.
-Check I.
 Goal True. idtac "File ""./example_075.v"", line 999, characters 0-0:". idtac "Error:". Abort.
-Goal True.
-big 40%nat.
-Abort.
+Eval cbv in seq 0 (10 * 1000).
